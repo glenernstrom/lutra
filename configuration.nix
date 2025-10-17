2cbc2681d20ecda0b861e1b147a1b347ca670d79
Glen Ernstrom <otterbrain@protonmail.com>
2025-10-16 21:35:54 -0400

    nix flake updated

--- configuration.nix
+++ configuration.nix
@@ -156,9 +156,9 @@
   # Nvidia graphics settings
   hardware.graphics.enable = true;
   services.xserver.videoDrivers = ["nvidia"];
- # hardware.nvidia.modesetting.enable = true;
- # hardware.nvidia.powermanagement.enable = true;
-  hardware.nvidia.open = false;
+  hardware.nvidia.modesetting.enable = true;
+  hardware.nvidia.powerManagement.enable = true;
+  hardware.nvidia.open = true;
   hardware.nvidia.nvidiaSettings = true;
   hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.beta;
 
